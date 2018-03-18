a938ac7c13eb7695a98f794ac64e4dac42845a7d
elishakay <kramer1346@gmail.com>
2018-03-18 16:03:47 +0200

    added one cross mark into the checklist array

--- content.js
+++ content.js
@@ -5,13 +5,15 @@
 // This is the content script for the extension
 
 var sidebar = "<div class='sidebar'> <ul class='sidebar-list'>"
-sidebar += "<li class='sidebar-item'><a href='#' class='sidebar-anchor'>Item 1</a></li>" 
-sidebar += "<li class='sidebar-item'><a href='#' class='sidebar-anchor'>Item 2</a></li>"
-sidebar += "<li class='sidebar-item'><a href='#' class='sidebar-anchor'>Item 3</a></li>"
-sidebar += "<li class='sidebar-item'><a href='#' class='sidebar-anchor'>Item 4</a></li>"
+sidebar += "<li class='sidebar-item'><p class='sidebar-anchor'>Item 1</p></li>" 
+sidebar += "<li class='sidebar-item'><p class='sidebar-anchor'>Item 2</p></li>"
+sidebar += "<li class='sidebar-item'><p class='sidebar-anchor'>Item 3</p></li>"
+sidebar += "<li class='sidebar-item'><p class='sidebar-anchor'>Item 4</p></li>"
 sidebar += "</ul></div>";
 
+var check = "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 130.2 130.2'> <circle class='path circle' fill='none' stroke='#73AF55' stroke-width='6' stroke-miterlimit='10' cx='65.1' cy='65.1' r='62.1'/> <polyline class='path check' fill='none' stroke='#73AF55' stroke-width='6' stroke-linecap='round' stroke-miterlimit='10' points='100.2,40.2 51.5,88.8 29.8,67.5 '/> </svg> <p class='success'>Oh Yeah!</p>";
 
+var cross = "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 130.2 130.2'> <circle class='path circle' fill='none' stroke='#D06079' stroke-width='6' stroke-miterlimit='10' cx='65.1' cy='65.1' r='62.1'/> <line class='path line' fill='none' stroke='#D06079' stroke-width='6' stroke-linecap='round' stroke-miterlimit='10' x1='34.4' y1='37.9' x2='95.8' y2='92.3'/> <line class='path line' fill='none' stroke='#D06079' stroke-width='6' stroke-linecap='round' stroke-miterlimit='10' x1='95.8' y1='38' x2='34.4' y2='92.2'/> </svg> <p class='error'>Bummer!</p>";
 
 window.onload = function() {
 
@@ -34,7 +36,21 @@ window.onload = function() {
     }
 
     toggleSidebar();
+
+    var checklist = $('.sidebar-item');
+    console.log('these are the checklist elements',checklist);
+
+    var crossSign = document.createElement('div');
+    crossSign.innerHTML = cross;
+
+    for(var i in checklist){
+    	var guideline = checklist[i];
+    	guideline.before(crossSign);
+    	// refElem.parentNode.insertBefore(elementToInsert.CloneNode(), refElem.nextSibling);
+	};
     
+    
+
     // $(".button").on("click tap", function() {
     //     toggleSidebar();
     // });
